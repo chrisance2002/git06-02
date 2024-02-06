0ea21b8dca9bbbc6bf00af40b1a29cf7af690935
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-06 15:45:13 +0530

    firstch

--- circle.java
+++ circle.java
@@ -2,7 +2,7 @@ public class circle {
     public static void main(String[] args){
         int r=6;
         float pi=3.14f;
-        float area=pi*r*r;
+        float areaaaa=pi*r*r;
         float perimeter=2*pi*r;
         System.out.println("Area of circle:"+area);
         System.out.println("peri of a circle:"+perimeter);
