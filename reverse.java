8d2cea80b6fc74adcff54a7bf6c4c8a881a4b194
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-06 13:51:28 +0530

    frscom

---
+++ reverse.java
@@ -0,0 +1,18 @@
+
+public class reverse {
+
+    public static void  main(String[] args){
+        String D="Dhanya";
+        String reverse="";
+        System.out.println("My name:"+D);
+        for(int i=0;i<D.length();i++){
+            reverse=D.charAt(i)+reverse;
+        }
+        System.out.println("Reversed string is:"+reverse);
+
+    }
+
+
+        
+    }
+    
